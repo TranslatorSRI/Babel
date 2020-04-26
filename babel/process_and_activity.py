caf17cf1218a5c3b82860e24cb1ca806789efb2f
cbizon <bizon@renci.org>
2020-04-26 13:57:49 -0400

    Updated process/activity to handle new label approach

--- babel/process_and_activity.py
+++ babel/process_and_activity.py
@@ -1,5 +1,5 @@
 from babel.ubergraph import UberGraph
-from src.LabeledID import LabeledID
+#from src.LabeledID import LabeledID
 from src.util import Text
 from babel.babel_utils import write_compendium,glom,get_prefixes,clean_sets
 from collections import defaultdict
@@ -11,38 +11,40 @@ def build_sets(iri, ignore_list = ['PMID','EC']):
     uberres = uber.get_subclasses_and_xrefs(iri)
     results = []
     uberres = clean_sets(uberres)
+    labels = {}
     for k,v in uberres.items():
         dbx = set([ x for x in v if not Text.get_curie(x) in ignore_list ])
         #prefixes = set([Text.get_curie(x) for x in dbx])
         #print(prefixes)
-        dbx.add(LabeledID(identifier=k[0],label=k[1]))
+        dbx.add(k[0])
         results.append(dbx)
-    return results
+        labels[k[0]] = k[1]
+    return results,labels
 
 def load_one(starter,stype):
-    sets = build_sets(starter)
-    relabel_entities(sets)
+    sets,labels = build_sets(starter)
+    #relabel_entities(sets)
     dicts = {}
     glom(dicts, sets,unique_prefixes=['GO'])
     osets = set([frozenset(x) for x in dicts.values()])
-    write_compendium(osets,f'{stype}.txt',stype)
+    write_compendium(osets,f'{stype}.txt',stype,labels=labels)
 
 def load():
     load_one('GO:0003674','molecular_activity')
     load_one('GO:0008150','biological_process')
 
 
-def relabel_entities(sets):
-    curie_to_labeledID = {}
-    for s in sets:
-        for si in s:
-            if isinstance(si,LabeledID):
-                curie_to_labeledID[si.identifier] = si
-    for s in sets:
-        for si in s:
-            if si in curie_to_labeledID:
-                s.remove(si)
-                s.add(curie_to_labeledID[si])
+#def relabel_entities(sets):
+#    curie_to_labeledID = {}
+#    for s in sets:
+#        for si in s:
+#            if isinstance(si,LabeledID):
+#                curie_to_labeledID[si.identifier] = si
+#    for s in sets:
+#        for si in s:
+#            if si in curie_to_labeledID:
+#                s.remove(si)
+#                s.add(curie_to_labeledID[si])
 
 if __name__ == '__main__':
     load()
