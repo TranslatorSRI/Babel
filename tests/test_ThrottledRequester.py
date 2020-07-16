1ca673a5d55f4ffc2ea58e1d4b1c6b40ceffb70d
cbizon <bizon@renci.org>
2020-07-16 12:33:38 -0400

    changing timing

--- tests/test_ThrottledRequester.py
+++ tests/test_ThrottledRequester.py
@@ -7,7 +7,8 @@ def test_throttling():
     """Call a quick-returning service, but include a throttle of 1/2 second (500 ms).
     This should end up taking just a bit over 500 ms.  The service being called
     just returns an empty json.  It's not immediate, but it returns quick enough that
-    a 1/2 second throttle will be invoked."""
+    a 1/2 second throttle will be invoked.
+    The test is a little goofy, and the half_sec_plus number is made up"""
     tr = ThrottledRequester(500)
     now = dt.now()
     response,throttle1 = tr.get('http://www.mocky.io/v2/5df243b23100007f009a31b0')
@@ -15,7 +16,7 @@ def test_throttling():
     later = dt.now()
     runtime = later - now
     half_sec = timedelta(milliseconds = 500)
-    half_sec_plus = timedelta(milliseconds = 800)
+    half_sec_plus = timedelta(milliseconds = 810)
     assert not throttle1 #don't throttle the first time through
     assert throttle2     #do throttle the second time through
     assert runtime > half_sec
