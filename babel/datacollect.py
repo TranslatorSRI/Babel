6100e2eca4f4769bcabd17f984a96ebb4b8c5be4
Chris Bizon <bizon@rciadmins-mbp.lan>
2021-03-17 09:28:28 -0400

    added hgnc synonyms

--- babel/datacollect.py
+++ babel/datacollect.py
@@ -2,6 +2,7 @@ from babel.ubergraph import UberGraph
 from babel.babel_utils import make_local_name, pull_via_ftp
 from collections import defaultdict
 import os, gzip
+from json import loads,dumps
 
 def pull_uber_labels():
     uber = UberGraph()
@@ -136,9 +137,34 @@ def pull_pubchem():
     pull_pubchem_labels()
     pull_pubchem_synonyms()
 
+def pull_hgnc():
+    data = pull_via_ftp('ftp.ebi.ac.uk', '/pub/databases/genenames/new/json', 'hgnc_complete_set.json')
+    hgnc_json = loads(data)
+    lname = make_local_name('labels', subpath='HGNC')
+    sname = make_local_name('synonyms', subpath='HGNC')
+    with open(lname,'w') as lfile, open(sname,'w') as sfile:
+        for gene in hgnc_json['response']['docs']:
+            hgnc_id =gene['hgnc_id']
+            symbol = gene['symbol']
+            lfile.write(f'{hgnc_id}\t{symbol}\n')
+            name = gene['name']
+            sfile.write(f'{hgnc_id}\thttp://www.geneontology.org/formats/oboInOwl#hasExactSynonym\t{name}\n')
+            if 'alias_symbol' in gene:
+                print('alias symbol')
+                alias_symbols = gene['alias_symbol']
+                for asym in alias_symbols:
+                    sfile.write(f'{hgnc_id}\thttp://www.geneontology.org/formats/oboInOwl#hasRelatedSynonym\t{asym}\n')
+            if 'alias_name' in gene:
+                print('alias name')
+                alias_names = gene['alias_name']
+                for asym in alias_names:
+                    sfile.write(f'{hgnc_id}\thttp://www.geneontology.org/formats/oboInOwl#hasRelatedSynonym\t{asym}\n')
+
+
 if __name__ == '__main__':
-    pull_ubers()
-    pull_mesh_labels()
-    pull_umls()
-    pull_pubchem()
+    #pull_ubers()
+    #pull_mesh_labels()
+    #pull_umls()
+    #pull_pubchem()
+    pull_hgnc()
 
