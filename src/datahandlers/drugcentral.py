f45c886401b63324fb0b875ec435af30a6c98dc2
Chris Bizon <bizon@email.unc.edu>
2021-10-21 13:08:56 -0400

    updated url

--- src/datahandlers/drugcentral.py
+++ src/datahandlers/drugcentral.py
@@ -2,7 +2,7 @@ from src.prefixes import DRUGCENTRAL
 from src.babel_utils import pull_via_urllib
 
 def pull_drugcentral():
-    dname = pull_via_urllib('https://unmtid-shinyapps.net/download/','structures.smiles.tsv',decompress=False,subpath='DRUGCENTRAL')
+    dname = pull_via_urllib('https://unmtid-shinyapps.net/download/20200516','structures.smiles.tsv',decompress=False,subpath='DRUGCENTRAL')
 
 def make_labels(infile,outfile):
     with open(infile,'r') as inf, open(outfile,'w') as outf:
