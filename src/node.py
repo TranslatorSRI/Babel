404254b6792d72b17d66c1340d92ea33a5bce7b4
Chris Bizon <bizon@email.unc.edu>
2021-10-26 09:47:32 -0400

    src/node.py

--- src/node.py
+++ src/node.py
@@ -95,6 +95,9 @@ class NodeFactory:
             prefs = self.get_prefixes("biolink:SmallMolecule")
         elif input_type == 'biolink:Polypeptide':
             prefs = prefs + self.get_prefixes('biolink:SmallMolecule')
+        elif input_type == 'biolink:ChemicalEntity':
+            #This just has to be here for now
+            prefs = prefs + self.get_prefixes('biolink:SmallMolecule')
         #The pref are in a particular order, but apparently it can have dups (ugh)
         # The particular dups are gone now, but the code remains in case they come back...
         newprefs = ['']
