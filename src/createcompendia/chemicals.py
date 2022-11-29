1df7f3d204dcb3d933e448fa593ad97edeacbb80
Gaurav Vaidya <gaurav@renci.org>
2022-11-29 11:58:28 -0500

    Updated GtoPdb version line.

--- src/createcompendia/chemicals.py
+++ src/createcompendia/chemicals.py
@@ -191,7 +191,7 @@ def write_chemical_ids_from_labels_and_smiles(labelfile,smifile,outfile):
 def parse_smifile(infile,outfile,smicol,idcol,pref,stripquotes=False):
     with open(infile,'r') as inf, open(outfile,'w') as outf:
         for line in inf:
-            if line.startswith('"## GtoPdb Version'):
+            if line.startswith('"# GtoPdb Version'):
                 # Header line! Ignore.
                 continue
             x = line.split('\t')
