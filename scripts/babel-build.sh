aa3352d47ceef3b1708a966d8a3c9620022a90fb
Gaurav Vaidya <gaurav@renci.org>
2022-11-28 22:35:40 -0500

    Added a babel-build.sh script.

---
+++ scripts/babel-build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# This script can be used to configure and run snakemake.
+#
+
+# Number of cores to use.
+export CORES=5
+
+# Dry run: if true, run Snakemake in a dry run.
+export DRY_RUN=1
+
+# Verbose: if set, produce verbose output.
+export VERBOSE=1
+
+# Keep going: if set, then keep going if one job errors out.
+export KEEP_GOING=1
+
+# Rerun incomplete: if set, then rerun any jobs that are incomplete.
+export RERUN_INCOMPLETE=1
+
+# Keep incomplete: if set, keep any incomplete files.
+export KEEP_INCOMPLETE=
+
+# Until: run until a particular rule or file.
+export UNTIL=
+
+# Determine the location of this script, so we can change to the right directory before running this script.
+export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR/.." || exit
+snakemake \
+  --cores ${CORES} \
+  "$([[ $DRY_RUN ]] && echo "--dry-run")" \
+  "$([[ $VERBOSE ]] && echo "--verbose")" \
+  "$([[ $KEEP_GOING ]] && echo "--keep-going")" \
+  "$([[ $RERUN_INCOMPLETE ]] && echo "--rerun-incomplete")" \
+  "$([[ $KEEP_INCOMPLETE ]] && echo "--keep-incomplete")" \
+  "$([[ $UNTIL ]] && echo "--until ${UNTIL}")"
