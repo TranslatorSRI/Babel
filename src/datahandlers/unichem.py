0e246d96ad970c0b0dc49197a241b1070b409017
Gaurav Vaidya <gaurav@renci.org>
2023-03-01 23:23:51 -0500

    Split pull_unichem from filter_unichem, improved assert().

--- src/datahandlers/unichem.py
+++ src/datahandlers/unichem.py
@@ -10,16 +10,17 @@ data_sources: dict = {'1': CHEMBLCOMPOUND, '2': DRUGBANK, '4': GTOPDB, '6': KEGG
 
 
 def pull_unichem():
+    """ Download UniChem files. """
     pull_via_urllib('http://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/table_dumps/', 'structure.tsv.gz', decompress=False, subpath='UNICHEM')
-    ref_file = pull_via_urllib('http://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/table_dumps/', 'reference.tsv.gz', decompress=False, subpath='UNICHEM')
-    ref_path, filename = os.path.split(ref_file)
-    ref_filtered = os.path.join(ref_path, 'reference.filtered.tsv.gz')
+    pull_via_urllib('http://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/table_dumps/', 'reference.tsv.gz', decompress=False, subpath='UNICHEM')
 
+def filter_unichem(ref_file, ref_filtered):
+    """ Filter UniChem reference file to those sources we're interested in. """
     srclist = [str(k) for k in data_sources.keys()]
     with gzip.open(ref_file, "rt") as rf, open(ref_filtered, "wt") as ref_filtered:
         header_line = rf.readline()
-        assert(header_line == "UCI\tSRC_ID\tSRC_COMPOUND_ID\tASSIGNMENT\n")
-        ref_filtered.writelines([header_line])
+        assert(header_line == "UCI\tSRC_ID\tSRC_COMPOUND_ID\tASSIGNMENT\n", f"Incorrect header line in {ref_file}: {header_line}")
+        ref_filtered.write(header_line)
         for line in rf:
             x = line.rstrip().split('\t')
             if x[1] in srclist and x[3] == '1':
