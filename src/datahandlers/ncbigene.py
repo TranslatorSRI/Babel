92bbb2ff4f644eebffc8e3d03cee7122b0ba09b3
Gaurav Vaidya <gaurav@renci.org>
2023-07-07 15:42:08 -0400

    Added helper function to replace `-` with blank string.

--- src/datahandlers/ncbigene.py
+++ src/datahandlers/ncbigene.py
@@ -17,8 +17,8 @@ def pull_ncbigene_labels_and_synonyms():
     with gzip.open(ifname, 'r') as inf, open(labelname, 'w') as labelfile, open(synname, 'w') as synfile:
 
         # Make sure the gene_info.gz columns haven't changed from under us.
-        header = inf.readline()
-        assert header.split("\t") == [
+        header = inf.readline().decode('utf-8').strip().split("\t")
+        assert header == [
             "#tax_id",
             "GeneID",
             "Symbol",
@@ -36,24 +36,39 @@ def pull_ncbigene_labels_and_synonyms():
             "Modification_date",
             "Feature_type"]
 
+        def get_field(row, field_name):
+            """
+            A helper function for returning the value of a field in gene_info.gz by name.
+            The value is `-` if no value is present; we need to convert that into an empty string.
+
+            :param row: A row from gene_info.gz.
+            :param field_name: A field name in the header of gene_info.gz.
+            :return: The value in this column in this row, otherwise the empty string ('').
+            """
+            index = header.index(field_name)
+            value = row[index].strip()
+            if value == '-':
+                return ''
+            return value
+
         for line in inf:
             sline = line.decode('utf-8')
             row = sline.strip().split('\t')
-            gene_id = f'NCBIGene:{row[1]}'
-            symbol = row[header.index("Symbol")]
-            gene_type = row[header.index("type_of_gene")]
+            gene_id = f'NCBIGene:{get_field(row, "GeneID")}'
+            symbol = get_field(row, "Symbol")
+            gene_type = get_field(row, "type_of_gene")
             if gene_type in bad_gene_types:
                 continue
             labelfile.write(f'{gene_id}\t{symbol}\n')
-            syns = set(row[header.index("Synonyms")].split('|'))
+            syns = set(get_field(row, "Synonyms").split('|'))
             syns.add(symbol)
-            description = row[header.index("description")]
+            description = get_field(row, "description")
             syns.add(description)
-            authoritative_symbol = row[header.index("Symbol_from_nomenclature_authority")]
+            authoritative_symbol = get_field(row, "Symbol_from_nomenclature_authority")
             syns.add(authoritative_symbol)
-            authoritative_full_name = row[header.index("Full_name_from_nomenclature_authority")]
+            authoritative_full_name = get_field(row, "Full_name_from_nomenclature_authority")
             syns.add(authoritative_full_name)
-            others = set(row[header.index("Other_designations")].split('|'))
+            others = set(get_field(row, "Other_designations").split('|'))
             syns.update(others)
             for syn in syns:
                 if syn == '-':
