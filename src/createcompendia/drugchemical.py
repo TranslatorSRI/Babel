aec77e5416af3065c3ac09e16cec5c2de9f32696
cbizon <bizon@renci.org>
2023-07-25 15:25:07 -0400

    Fixed PUBCHEM/RXN conflation

--- src/createcompendia/drugchemical.py
+++ src/createcompendia/drugchemical.py
@@ -224,30 +224,39 @@ def build_conflation(rxn_concord,pubchem_rxn_concord,drug_compendium,chemical_co
         print(f"load {chemical_compendium}")
         chemical_rxcui_to_clique.update(load_cliques(chemical_compendium))
     pairs = []
-    for conc in [rxn_concord,pubchem_rxn_concord]:
-        print(f"load {conc}")
-        with open(conc,"r") as infile:
-            for line in infile:
-                x = line.strip().split('\t')
-                subject = x[0]
-                object = x[2]
-                if subject in drug_rxcui_to_clique and object in chemical_rxcui_to_clique:
-                    subject = drug_rxcui_to_clique[subject]
-                    object = chemical_rxcui_to_clique[object]
-                    pairs.append( (subject,object) )
-                elif subject in chemical_rxcui_to_clique and object in drug_rxcui_to_clique:
-                    subject = chemical_rxcui_to_clique[subject]
-                    object = drug_rxcui_to_clique[object]
-                    pairs.append( (subject,object) )
-                # OK, this is possible, and it's OK, as long as we get real clique leaders
-                elif subject in drug_rxcui_to_clique and object in drug_rxcui_to_clique:
-                    subject = drug_rxcui_to_clique[subject]
-                    object = drug_rxcui_to_clique[object]
-                    pairs.append( (subject,object) )
-                elif subject in chemical_rxcui_to_clique and object in chemical_rxcui_to_clique:
-                    subject = chemical_rxcui_to_clique[subject]
-                    object = chemical_rxcui_to_clique[object]
-                    pairs.append( (subject,object) )
+    with open(rxn_concord,"r") as infile:
+        for line in infile:
+            x = line.strip().split('\t')
+            subject = x[0]
+            object = x[2]
+            if subject in drug_rxcui_to_clique and object in chemical_rxcui_to_clique:
+                subject = drug_rxcui_to_clique[subject]
+                object = chemical_rxcui_to_clique[object]
+                pairs.append( (subject,object) )
+            elif subject in chemical_rxcui_to_clique and object in drug_rxcui_to_clique:
+                subject = chemical_rxcui_to_clique[subject]
+                object = drug_rxcui_to_clique[object]
+                pairs.append( (subject,object) )
+            # OK, this is possible, and it's OK, as long as we get real clique leaders
+            elif subject in drug_rxcui_to_clique and object in drug_rxcui_to_clique:
+                subject = drug_rxcui_to_clique[subject]
+                object = drug_rxcui_to_clique[object]
+                pairs.append( (subject,object) )
+            elif subject in chemical_rxcui_to_clique and object in chemical_rxcui_to_clique:
+                subject = chemical_rxcui_to_clique[subject]
+                object = chemical_rxcui_to_clique[object]
+                pairs.append( (subject,object) )
+    with open(pubchem_rxn_concord,"r") as infile:
+        for line in infile:
+            x = line.strip().split('\t')
+            subject = x[0]
+            object = x[2]
+            #object is a PUBCHEM.  It's by definition a clique_leader.
+            if subject in drug_rxcui_to_clique:
+                subject = drug_rxcui_to_clique[subject]
+            elif subject in chemical_rxcui_to_clique:
+                subject = chemical_rxcui_to_clique[subject]
+            pairs.append((subject, object))
     print("glom")
     gloms = {}
     glom(gloms,pairs)
