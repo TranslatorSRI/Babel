f75508a00a286d48ef00438d97dd2039fb7d2a2d
Gaurav Vaidya <gaurav@renci.org>
2023-08-21 15:56:20 -0400

    Added code to identify cliques to be conflated.

--- bin/conflate-synonyms.py
+++ bin/conflate-synonyms.py
@@ -7,6 +7,7 @@ conflation files.
 import sys
 import json
 import logging
+from collections import defaultdict
 
 import click
 
@@ -18,13 +19,13 @@ logging.basicConfig(level=logging.INFO)
 @click.option('--conflation-file', multiple=True, type=click.File('r'))
 @click.option('--compendium-file', multiple=True, type=click.File('r'))
 @click.option('--output', type=click.File('w'), default=sys.stdout)
-@click.argument("synonym_file", type=click.File('r'))
-def conflate_synonyms(synonym_file, conflation_file, compendium_file, output):
+@click.argument("synonym_files", nargs=-1, type=click.File('r'))
+def conflate_synonyms(synonym_files, conflation_file, compendium_file, output):
     """
     Generate a synonym file based on a single input synonym, the conflation described in the input conflation files,
     and any cross-references present in the input compendia files.
 
-    :param synonym_file: The input synonym file.
+    :param synonym_files: The input synonym files.
     :param conflation_file: Any conflation files to apply.
     :param compendium_file: Any compendia files to incorporate cross-references from.
     :param output: The file to write the synonyms to.
@@ -49,8 +50,8 @@ def conflate_synonyms(synonym_file, conflation_file, compendium_file, output):
 
             # The conflation line is a list of identifiers, e.g. `["ID1", "ID2", "ID3"]`
             # Note that we map the primary identifier to itself.
-            for id in conflation:
-                add_conflation(conflation[0], id)
+            for ident in conflation:
+                add_conflation(conflation[0], ident)
                 count_secondary += 1
             count_primary += 1
 
@@ -59,28 +60,37 @@ def conflate_synonyms(synonym_file, conflation_file, compendium_file, output):
 
     logging.info(f"Loaded all conflation files, found {len(conflation_index):,} identifiers in total.")
 
-    # Step 2. For the conflations we've loaded, extend them to include all the identifiers in their clique.
+    # Step 2. Identify the cliques to be conflated, i.e. cliques that contain an identifier we have in the
+    # conflation index. Most cliques will end up being ignored.
+    count_cliques = 0
+    count_cliques_to_conflate = 0
+    cliques_to_be_conflated = defaultdict(list)
     for compendiumf in compendium_file:
-        count_cliques = 0
-        count_ids_added = 0
+        count_file_cliques = 0
+        count_file_cliques_to_conflate = 0
         for line in compendiumf:
             compendium = json.loads(line)
-            identifiers = map(lambda id: id['i'], compendium.get('identifiers', []))
+            identifiers = map(lambda identifier: identifier['i'], compendium.get('identifiers', []))
 
-            for id in identifiers:
-                if id in conflation_index:
-                    # This clique contains an identifier we conflate! We set them to the same primary ID.
-                    primary_id = conflation_index[id]
-                    for id_inner in identifiers:
-                        if id_inner not in conflation_index or conflation_index[id_inner] != primary_id:
-                            add_conflation(primary_id, id_inner)
-                            count_ids_added += 1
-                    count_cliques += 1
-                    break
+            for ident in identifiers:
+                if ident in conflation_index:
+                    # Add this to the list of the cliques to be conflated.
+                    id_to_conflate_to = conflation_index[ident]
+                    cliques_to_be_conflated[id_to_conflate_to].append(compendium)
+                    count_cliques_to_conflate += 1
+                    count_file_cliques_to_conflate += 1
+                count_cliques += 1
+                count_file_cliques += 1
 
-        logging.info(f"Added {count_cliques} cliques and {count_ids_added} IDs added from compendium file {compendiumf.name}.")
+        logging.info(f"Identified {count_file_cliques_to_conflate} cliques to conflate out of {count_file_cliques} ({(count_file_cliques_to_conflate/count_file_cliques*100):.2%}) in {compendiumf.name}.")
 
-    logging.info(f"Loaded all compendium files, found {len(conflation_index):,} identifiers in total.")
+    logging.info(f"Identified {count_cliques_to_conflate} cliques to be conflated out of {count_cliques} cliques ({(count_cliques_to_conflate/count_cliques*100):.2%}) considered in compendia.")
+
+    # Step 3. Conflate the synonyms.
+    for synonymsf in synonym_files:
+        for synonym in synonymsf:
+            # Do we need to conflate this synonym at all?
+
 
 
 if __name__ == '__main__':
