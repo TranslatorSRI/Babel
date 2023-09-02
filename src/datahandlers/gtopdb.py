85a3cbb1019f58e74536e6186a80f45f3e1b33a3
Gaurav Vaidya <gaurav@renci.org>
2023-09-02 16:52:46 -0400

    Added code for stripping HTML entities from label and synonym generation.

--- src/datahandlers/gtopdb.py
+++ src/datahandlers/gtopdb.py
@@ -1,8 +1,26 @@
 from src.prefixes import GTOPDB
 from src.babel_utils import pull_via_urllib
 
+from bs4 import BeautifulSoup
+
 def pull_gtopdb_ligands():
     pull_via_urllib('https://www.guidetopharmacology.org/DATA/','ligands.tsv',decompress=False,subpath='GTOPDB')
+
+
+def strip_html_tags(name):
+    """
+    GtoPDB contains HTML tags, such as <sup>, </sup>, &Delta;, &beta; and others.
+
+    (e.g. GTOPDB:4215 has a label of "Hg<sup>2+</sup>", see
+    https://www.guidetopharmacology.org/GRAC/LigandDisplayForward?ligandId=4215)
+
+    This function will strip all those HTML tags.
+
+    :param name: The input label or synonym.
+    :return: The same label or synonym, but with the HTML tags stripped.
+    """
+    return BeautifulSoup(name, 'html.parser').get_text()
+
 
 def make_labels_and_synonyms(inputfile,labelfile,synfile):
     idcol = 0
@@ -17,8 +35,8 @@ def make_labels_and_synonyms(inputfile,labelfile,synfile):
             label = parts[labelcol][1:-1]
             synstring = parts[syncol][1:-1]
             if len(label) > 0:
-                lf.write(f'{ident}\t{label}\n')
+                lf.write(f'{ident}\t{strip_html_tags(label)}\n')
             if len(synstring) > 0:
                 syns = synstring.split('|')
                 for syn in syns:
-                    sf.write(f'{ident}\toio:exact\t{syn}\n')
+                    sf.write(f'{ident}\toio:exact\t{strip_html_tags(syn)}\n')
