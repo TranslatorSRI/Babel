87df665b809f75f8778ebae9b683b99b59a9ab21
Gaurav Vaidya <gaurav@renci.org>
2023-12-10 02:26:37 -0500

    Changed warning for no preferred name to debug.

--- src/babel_utils.py
+++ src/babel_utils.py
@@ -404,7 +404,7 @@ def write_compendium(synonym_list,ofname,node_type,labels={},extra_prefixes=[],i
                     if filtered_possible_labels:
                         document["preferred_name"] = filtered_possible_labels[0]
                     else:
-                        logging.warning(f"No preferred name for {node}")
+                        logging.debug(f"No preferred name for {node}")
 
                     # We previously used the shortest length of a name as a proxy for how good a match it is, i.e. given
                     # two concepts that both have the word "acetaminophen" in them, we assume that the shorter one is the
