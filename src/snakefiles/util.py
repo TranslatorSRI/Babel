d5760998560e7470bbde1e1a4e894269919a9b1f
Gaurav Vaidya <gaurav@renci.org>
2024-03-28 12:48:40 -0400

    Updated synonym test to look for duplicated synonym files.

--- src/snakefiles/util.py
+++ src/snakefiles/util.py
@@ -14,8 +14,14 @@ def get_all_compendia(config):
             config['macromolecularcomplex_outputs'])
 
 
-# List of all the synonym files, except DrugChemicalConflated.
-def get_synonyms(config):
+def get_all_synonyms(config):
+    """
+    List of all the synonym files, including DrugChemicalConflated. Note that this duplicates synonyms: chemical output
+    synonyms will be in both the individual chemical outputs and the DrugChemicalConflated file.
+
+    :param config: The Babel config to use.
+    :return: A list of filenames expected in the `synonyms/` directory.
+    """
     return (
         config['anatomy_outputs'] +
         config['gene_outputs'] +
@@ -25,15 +31,41 @@ def get_synonyms(config):
         config['chemical_outputs'] +
         config['taxon_outputs'] +
         config['genefamily_outputs'] +
-        # config['drugchemicalconflated_synonym_outputs'] +
+        config['drugchemicalconflated_synonym_outputs'] +
         config['umls_outputs'] +
         config['macromolecularcomplex_outputs']
     )
 
 
-# List of all the synonym files including DrugChemicalConflated instead of the files it
-# duplicates.
+def get_all_synonyms_except_drugchemicalconflated(config):
+    """
+    List of all the synonym files, except DrugChemicalConflated.
+
+    :param config: The Babel config to use.
+    :return: A list of filenames expected in the `synonyms/` directory.
+    """
+    return (
+            config['anatomy_outputs'] +
+            config['gene_outputs'] +
+            config['protein_outputs'] +
+            config['disease_outputs'] +
+            config['process_outputs'] +
+            config['chemical_outputs'] +
+            config['taxon_outputs'] +
+            config['genefamily_outputs'] +
+            # config['drugchemicalconflated_synonym_outputs'] +
+            config['umls_outputs'] +
+            config['macromolecularcomplex_outputs']
+    )
+
+
 def get_all_synonyms_with_drugchemicalconflated(config):
+    """
+    List of all the synonym files including DrugChemicalConflated instead of the files it duplicates.
+
+    :param config: The Babel config to use.
+    :return: A list of filenames expected in the `synonyms/` directory.
+    """
     return (
             config['anatomy_outputs'] +
             config['gene_outputs'] +
