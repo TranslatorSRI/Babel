6d5f22dcbb928c44292074ff63937933902047fe
Gaurav Vaidya <gaurav@ggvaidya.com>
2024-08-13 00:20:23 -0600

    Improved error checking on Wikidata.

--- src/createcompendia/anatomy.py
+++ src/createcompendia/anatomy.py
@@ -108,6 +108,8 @@ def build_wikidata_cell_relationships(outdir):
         }"""
     frink_wikidata_url = "https://frink.apps.renci.org/federation/sparql"
     response = requests.post(frink_wikidata_url, params={'query': sparql})
+    if not response.ok():
+        raise RuntimeError(f"Could not query {frink_wikidata_url}: {response.status_code} {response.reason}")
     results = response.json()
     rows = results["results"]["bindings"]
     # If one wikidata entry has either more than one CL or more than one UMLS, then we end up with problems
