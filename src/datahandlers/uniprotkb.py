37d81a8c29e0beef248804be3c46005d5b7f031e
Gaurav Vaidya <gaurav@ggvaidya.com>
2024-10-16 17:10:26 -0400

    First stab at downloading UniProtKB mappings.

--- src/datahandlers/uniprotkb.py
+++ src/datahandlers/uniprotkb.py
@@ -1,3 +1,7 @@
+import csv
+import logging
+import os
+
 from src.babel_utils import pull_via_urllib, make_local_name
 
 
@@ -23,3 +27,41 @@ def pull_uniprot_labels(sprotfile,tremblfile,fname):
             labelfile.write(f'{k}\t{v}\n')
         for k,v in tlabels.items():
             labelfile.write(f'{k}\t{v}\n')
+
+
+def download_umls_gene_protein_mappings(umls_uniprotkb_raw_url, umls_uniprotkb_filename, umls_gene_concords, umls_protein_concords):
+    """
+    Chris Bizon generated a list of UMLS to NCBIGene/UniProtKB mappings in
+    https://github.com/cbizon/UMLS_UniProtKB. This function downloads this file
+    from that GitHub repository and generates concord files so that they can be
+    incorporated into our gene and protein cliques.
+
+    :param umls_uniprotkb_raw_url: The URL to download the UMLS/UniProtKB mapping file.
+    :param umls_uniprotkb_filename: The UMLS/UniProtKB filename to save the UMLS/UniProtKB filename.
+    :param umls_gene_concords: The file to write UMLS/NCBIGene gene concords to.
+    :param umls_uniprotkb_protein_concords: The file to write UMLS/UniProtKB protein concords to.
+    """
+
+    RELATION = 'oio:closeMatch'
+
+    # Step 1. Download the file.
+    pull_via_urllib(umls_uniprotkb_raw_url, umls_uniprotkb_filename, decompress=False)
+
+    # Step 2. Read the file into memory.
+    os.makedirs(os.path.dirname(umls_gene_concords), exist_ok=True)
+    os.makedirs(os.path.dirname(umls_protein_concords), exist_ok=True)
+
+    count_rows = 0
+    with open(umls_uniprotkb_filename, 'r') as f, \
+        open(umls_gene_concords, 'w') as genef, \
+        open(umls_protein_concords, 'w') as proteinf:
+        csv_reader = csv.DictReader(f, dialect='excel-tab')
+        for row in csv_reader:
+            count_rows += 1
+            if row.keys() != {'UMLS_protein', 'UMLS_gene', 'NCBI_gene', 'UniProtKB'}:
+                raise RuntimeError(f"Format of the UniProtKB download from {umls_uniprotkb_raw_url} has changed: {csv_reader.fieldnames}.")
+
+            genef.write(f"{row['UMLS_gene']}\t{RELATION}\t{row['NCBI_gene']}\n")
+            proteinf.write(f"{row['UMLS_protein']}\t{RELATION}\t{row['UniProtKB']}\n")
+
+    logging.info(f"Downloaded UMLS file from {umls_uniprotkb_raw_url} and added {count_rows} to gene and protein concords.")
