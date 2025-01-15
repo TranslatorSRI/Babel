3593f33f68c6e196f969b364b5ae7831346d8453
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-01-15 04:13:17 -0500

    Additional possible fixes.

--- src/createcompendia/drugchemical.py
+++ src/createcompendia/drugchemical.py
@@ -511,7 +511,7 @@ def build_conflation(manual_concord_filename, rxn_concord, umls_concord, pubchem
                 'identifiers': list(map(lambda curie: {'identifier': curie}, clique_for_preferred_curie[conflation_clique_leader]))
             }))
             if conflation_clique_leader_ic is None:
-                conflation_clique_leader_ic = 100
+                conflation_clique_leader_ic = 100.0
 
             for curie in final_conflation_id_list:
                 curie_prefix = curie.split(':')[0]
@@ -529,7 +529,7 @@ def build_conflation(manual_concord_filename, rxn_concord, umls_concord, pubchem
                 curie_ic = float(ic_factory.get_ic({
                     'identifiers': list(map(lambda curie: {'identifier': curie}, clique_for_preferred_curie[curie]))
                 }))
-                if curie_ic is not None and curie_ic < conflation_clique_leader_ic:
+                if curie_ic is not None and float(curie_ic) < float(conflation_clique_leader_ic):
                     logging.info(f"Found better IC with CURIE {curie} (IC {curie_ic}) than previous conflation clique "
                                  f"leader {final_conflation_id_list[0]} (IC {conflation_clique_leader_ic}).")
                     conflation_clique_leader = curie
