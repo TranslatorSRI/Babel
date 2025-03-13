6eecdbd531452954bb2ff78b8a66debbe3280e11
Gaurav Vaidya <gaurav@renci.org>
2025-03-13 17:04:46 -0400

    Improved synonymizing for NCBITaxon.

--- src/datahandlers/ncbitaxon.py
+++ src/datahandlers/ncbitaxon.py
@@ -1,3 +1,6 @@
+import logging
+from collections import defaultdict
+
 from src.babel_utils import pull_via_ftp
 from src.prefixes import NCBITAXON
 import tarfile
@@ -9,7 +12,11 @@ def make_labels_and_synonyms(infile,labelfile,synfile):
     taxtar = tarfile.open(infile,'r')
     f = taxtar.extractfile('names.dmp')
     l = f.readlines()
-    usedsyns= set()
+
+    # It would be nice to put together the scientific name and common name(s) for a particular taxon, so
+    # we put that into a dictionary and write them out separately later.
+    names_by_txid = {}
+
     with open(labelfile,'w') as labelf, open(synfile,'w') as outsyn:
         for line in l:
             sline = line.decode('utf-8').strip().split('|')
@@ -31,14 +38,77 @@ def make_labels_and_synonyms(infile,labelfile,synfile):
             #  670412 	authority                   "Lavandula bipinnata Kuntze, 1891"
             # 2503930 	scientific name             "Knoxia platycarpa"
 
+            ncbi_txid = f"{NCBITAXON}:{parts[0]}"
+            name = parts[1]
+
+            # Write down the txid.
+            if ncbi_txid not in names_by_txid:
+                names_by_txid[ncbi_txid] = {}
+
+            # Flag to write this entry as a synonym. Essentially we want to add everything we select
+            # below as a synonym (including scientific names, equivalent names and so on), but things
+            # we don't select should NOT be added as a synonym (e.g. acronym). So we set it to be True
+            # here, but then change it to False if
+            flag_write_as_synonym = True
+
             match name_class:
-                # Labels: we use the scientific name and common name.
-                case 'scientific name' | 'common name' | 'genbank common name':
-                    labelf.write(f'{NCBITAXON}:{parts[0]}\t{parts[1]}\n')
-                # Synonyms: we use taxonomic synonyms and equivalent names.
-                # The blast name also seems to be useful, so let's add that as well.
-                case 'synonym' | 'equivalent name' | 'blast name':
+                # Labels: we use the scientific name and common name, which we put together afterwards.
+                case 'scientific name':
+                    if 'scientific name' not in names_by_txid[ncbi_txid]:
+                        # We only write down the first one.
+                        names_by_txid[ncbi_txid]['scientific name'] = name
+                    else:
+                        logging.warning(f"Found additional scientific name for {ncbi_txid}: '{name}' (previously '{names_by_txid[ncbi_txid]['scientific name']}'), ignoring.")
+
+                case 'common name':
+                    if 'common name' not in names_by_txid[ncbi_txid]:
+                        # We only write down the first one as the "official" common name.
+                        names_by_txid[ncbi_txid]['common name'] = name
+                    else:
+                        logging.warning(f"Found additional common name for {ncbi_txid}: '{name}' (previously '{names_by_txid[ncbi_txid]['common name']}'), ignoring.")
+
+                    # However, we should write all common names as synonyms!
+                    outsyn.write(f'{ncbi_txid}\toio:exactSynonym\t{name}\n')
+
+                case 'genbank common name':
+                    if 'genbank common name' not in names_by_txid[ncbi_txid]:
+                        # We only write down the first one as the "official" common name.
+                        names_by_txid[ncbi_txid]['genbank common name'] = name
+                    else:
+                        logging.warning(f"Found additional GenBank common name for {ncbi_txid}: '{name}' (previously '{names_by_txid[ncbi_txid]['genbank common name']}'), ignoring.")
+
+                # Synonyms: we use taxonomic synonyms and equivalent names, which are directly added as a synonym.
+                case 'synonym' | 'equivalent name':
                     # We previously uniquified the synonyms, but I don't think that's useful, because we can't really
                     # control which one gets the first synonym (I guess it's the smallest identifier)
-                    outsyn.write(f'{NCBITAXON}:{parts[0]}\toio:exactSynonym\t{parts[1]}\n')
+                    pass
 
+                # For every other case, we DON'T want to write this as a synonym.
+                case _:
+                    flag_write_as_synonym = False
+
+            # If this name should be written out as a synonym, then do so.
+            if flag_write_as_synonym:
+                outsyn.write(f'{ncbi_txid}\toio:exactSynonym\t{name}\n')
+
+        # Now that we've read the full taxdump file, let's write out all the labels.
+        for txid in names_by_txid:
+            scientific_name = names_by_txid[txid].get('scientific name', '')
+            common_name = names_by_txid[txid].get('common name', '')
+            genbank_common_name = names_by_txid[txid].get('genbank common name', '')
+
+            # We prefer the GenBank common name, if there is one.
+            if scientific_name and genbank_common_name:
+                labelf.write(f'{txid}\t{scientific_name} ({genbank_common_name})\n')
+            # Otherwise, the first common name is fine.
+            elif scientific_name and common_name:
+                labelf.write(f'{txid}\t{scientific_name} ({common_name})\n')
+            # Fall back to using whichever name is available.
+            elif scientific_name:
+                labelf.write(f'{txid}\t{scientific_name}\n')
+            elif genbank_common_name:
+                labelf.write(f'{txid}\t{genbank_common_name}\n')
+            elif common_name:
+                labelf.write(f'{txid}\t{common_name}\n')
+            else:
+                logging.warning(f"No scientific or common name found for {txid}, skipping.")
