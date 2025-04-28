c161728715dfef31396ea66e72d0b52537f6bba2
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-04-27 20:41:22 -0400

    Moved Ubergraph label/desc/syn downloads to a new directory.

--- src/datahandlers/obo.py
+++ src/datahandlers/obo.py
@@ -14,7 +14,7 @@ def pull_uber_icRDF(icrdf_filename):
     uber = UberGraph()
     _ = uber.write_normalized_information_content(icrdf_filename)
 
-def pull_uber_labels(expected):
+def pull_uber_labels(outputfile):
     uber = UberGraph()
     labels = uber.get_all_labels()
     ldict = defaultdict(set)
@@ -22,14 +22,14 @@ def pull_uber_labels(expected):
         iri = unit['iri']
         p = iri.split(':')[0]
         ldict[p].add( ( unit['iri'], unit['label'] ) )
-    for p in ldict:
-        if p not in ['http','ro'] and not p.startswith('t') and not '#' in p:
-            fname = make_local_name('labels',subpath=p)
-            with open(fname,'w') as outf:
+
+    with open(outputfile, 'w') as outf:
+        for p in ldict:
+            if p not in ['http','ro'] and not p.startswith('t') and '#' not in p:
                 for unit in ldict[p]:
                     outf.write(f'{unit[0]}\t{unit[1]}\n')
 
-def pull_uber_descriptions(expected):
+def pull_uber_descriptions(outputfile):
     uber = UberGraph()
     labels = uber.get_all_descriptions()
     ldict = defaultdict(set)
@@ -37,15 +37,15 @@ def pull_uber_descriptions(expected):
         iri = unit['iri']
         p = iri.split(':')[0]
         ldict[p].add( ( unit['iri'], unit['description'] ) )
-    for p in ldict:
-        if p not in ['http','ro'] and not p.startswith('t') and not '#' in p:
-            fname = make_local_name('descriptions',subpath=p)
-            with open(fname,'w') as outf:
+
+    with open(outputfile, 'w') as outf:
+        for p in ldict:
+            if p not in ['http','ro'] and not p.startswith('t') and '#' not in p:
                 for unit in ldict[p]:
                     outf.write(f'{unit[0]}\t{unit[1]}\n')
 
 
-def pull_uber_synonyms(expected):
+def pull_uber_synonyms(outputfile):
     uber = UberGraph()
     labels = uber.get_all_synonyms()
     ldict = defaultdict(set)
@@ -53,12 +53,10 @@ def pull_uber_synonyms(expected):
         iri = unit[0]
         p = iri.split(':')[0]
         ldict[p].add(  unit )
-    #There are some of the ontologies that we don't get synonyms for.   But this makes snakemake unhappy so
-    # we are going to make some zero-length files for it
-    for p in expected:
-        if p not in ['http','ro'] and not p.startswith('t') and not '#' in p:
-            fname = make_local_name('synonyms',subpath=p)
-            with open(fname,'w') as outf:
+
+    with open(outputfile, 'w') as outf:
+        for p in ldict:
+            if p not in ['http','ro'] and not p.startswith('t') and '#' not in p:
                 for unit in ldict[p]:
                     outf.write(f'{unit[0]}\t{unit[1]}\t{unit[2]}\n')
 
