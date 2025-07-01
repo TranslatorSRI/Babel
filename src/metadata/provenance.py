abb5aa9488e811c74178c47725348c280d6a88d0
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-07-01 00:48:53 -0400

    Fixed sources.

--- src/metadata/provenance.py
+++ src/metadata/provenance.py
@@ -2,13 +2,15 @@ from datetime import datetime
 
 import yaml
 
-def write_download_metadata(filename, name, url='', description='', sources=[]):
+def write_download_metadata(filename, name, url='', description='', sources=None):
     write_metadata(filename, 'download', name, url=url, description=description, sources=sources)
 
-def write_concord_metadata(filename, name, url='', description='', sources=[]):
+def write_concord_metadata(filename, name, url='', description='', sources=None):
     write_metadata(filename, 'concord', name, url=url, description=description, sources=sources)
 
-def write_metadata(filename, typ, name, sources=[], url='', description=''):
+def write_metadata(filename, typ, name, sources=None, url='', description=''):
+    if sources is None:
+        sources = []
     with open(filename, 'w') as fout:
         yaml.dump({
             'created_at': datetime.now().isoformat(),
